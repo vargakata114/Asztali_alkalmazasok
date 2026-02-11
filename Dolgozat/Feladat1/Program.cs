813ade27bdce8c4561aa76c1c6caa3b67800a0a5
vargakata114 <vargakata819@gmail.com>
2026-02-11 11:54:45 +0100

    2026.02.11

---
+++ Dolgozat/Feladat1/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Feladat1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int elso = 0;
+            int masodik = 0;
+            string inputText = null;
+            Console.WriteLine("Egész számok hányadosának kiszámítása");
+            Console.WriteLine("Kérem az első egész számot: ");
+            inputText = Console.ReadLine();
+            //elso = int.Parse(inputText);
+            int.TryParse(inputText, out elso);
+            elso = Convert.ToInt32(inputText);
+            Console.WriteLine("Kérem a második egész számot: ");
+            inputText = Console.ReadLine();
+            masodik = Convert.ToInt32(inputText);
+            Console.WriteLine($"A számok hányadosa: {(double)elso/masodik:F2} ");
+        }
+    }
+}
---
+++ Dolgozat/Feladat2/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace Feladat2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double a = 0, b = 0, c = 0;
+            double x1 = 0, x2 = 0;
+            string inputText = null;
+            Console.WriteLine("==== Másodfokú egyenlet megoldása ====");
+            Console.Write("Kérem az (A) együtthatót: ");
+            inputText = Console.ReadLine();
+            //a = double.Parse(inputText);
+            //a = Convert.ToDouble(inputText);
+            double.TryParse(inputText, out a);
+            Console.Write("Kérem a (B) eggyütthatót: ");
+            inputText = Console.ReadLine();
+            double.TryParse(inputText, out b);
+            Console.Write("Kérem a (C) eggyütthatót: ");
+            inputText = Console.ReadLine();
+            double.TryParse(inputText, out c);
+            x1 = (-1 * b + Math.Sqrt(b * b - 4 * a * c)) / 2 * a;
+            x2 = (-1 * b + Math.Sqrt(b * b - 4 * a * c)) / 2 * a;
+            Console.WriteLine($"Az x1 megoldása: {x1}");
+            Console.WriteLine($"Az x2 megoldása: {x2}");
+        }
+    }
+}
