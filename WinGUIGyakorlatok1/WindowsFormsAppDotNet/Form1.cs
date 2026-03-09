fe69c81c639c1dd2f40704cbf1be0f44004d700e
vargakata114 <vargakata819@gmail.com>
2026-03-09 10:55:59 +0100

    Add files via upload

---
+++ WinGUIGyakorlatok1/WindowsFormsAppDotNet/Form1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsAppDotNet
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Vezetéknév: " + textBox1.Text + " \n" +
+                            "Keresztnév: " + textBox1.Text + " \n" +
+                            "E-mail: " + textBox1.Text + " \n" +
+                            "Kor: " + textBox1.Text + " \n" +
+                            "Férfi: " + radioButton1.Checked + " \n" +
+                            "Nő: " + radioButton2.Checked + " \n" +
+                            "Értesíts: " + checkBox1.Checked + " \n");
+        }
+    }
+}
