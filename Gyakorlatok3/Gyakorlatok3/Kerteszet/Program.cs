50561cf4837e367cb6b0e841c91892fd627241ab
vargakata114 <vargakata819@gmail.com>
2026-03-09 10:45:05 +0100

    Add files via upload

---
+++ Gyakorlatok3/Gyakorlatok3/Kerteszet/Program.cs
@@ -0,0 +1,91 @@
+﻿using System.Linq.Expressions;
+
+namespace Kerteszet
+{
+    interface INoveny
+    {
+        public const int VIZ_AR = 150; // A publicot nem kötelező kiírni, mert az interfacebe minden public alapból is
+        double NapiVizfogyasztas(); // Az abstract ugyan ez, alapból nem kell megadni, mert az lesz
+
+    }
+
+    public abstract class Noveny : INoveny
+    {
+        protected bool ritka;
+        private double magassag;
+        public Noveny(double magassag, bool ritka)
+        {
+            this.magassag = magassag;
+            this.ritka = ritka;
+        }
+        public abstract void Novekszik();
+        public double NapiVizfogyasztas()
+        {
+            double alap = magassag * INoveny.VIZ_AR;
+            if (ritka == true) return alap * 1.5;
+            else return alap;
+        }
+
+        public override string ToString()
+        {
+            string statusz = ritka ? "Ritka" : "Közönséges";
+            return magassag + " m - " + statusz + " - " + NapiVizfogyasztas() + "Ft/Nap";
+        }
+
+    }
+
+    class RekaFA : Noveny
+    {
+        public RekaFA(double magassag, bool ritka) : base(magassag, ritka) { }
+        public override void Novekszik()
+        {
+            Console.WriteLine("A Réka fa lassan növekszik a vízparton.");
+        }
+    }
+
+    class Kaktusz : INoveny
+    {
+        private bool viragszoru;
+        public Kaktusz(bool viragszoru)
+        {
+            this.viragszoru = viragszoru;
+        }
+
+        public double NapiVizfogyasztas()
+        {
+            double szoro = viragszoru ? 0.3 : 0.1;
+            return szoro * INoveny.VIZ_AR;
+        }
+
+        public override string ToString()
+        {
+            string nev = viragszoru ? "Virágzó kaktusz" : "Normál kaktusz";
+            return nev + " - " + NapiVizfogyasztas() + "Ft/nap";
+        }
+    }
+
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<INoveny> lista = new List<INoveny>();
+
+            lista.Add(new RekaFA(15, false));
+            lista.Add(new RekaFA(12, true));
+            lista.Add(new Kaktusz(true));
+            lista.Add(new Kaktusz(false));
+
+            foreach (INoveny elem in lista)
+            {
+                Console.WriteLine(elem.ToString());
+                if (elem is RekaFA rekaFa)
+                {
+                    rekaFa.Novekszik();
+                }
+            }
+            Console.ReadLine();
+               
+;       }
+    }
+}
---
+++ Gyakorlatok3/Gyakorlatok3/nem/Program.cs
@@ -0,0 +1,101 @@
+﻿using System;
+
+namespace Dog
+{
+    public class Dog
+    {//Tulajdonságok (Properties)
+        public string Color { get; set; }
+        public string Name { get; set; }
+        public string Breed { get; set; }
+        public double Height { get; set; }
+        public double Weight { get; set; }
+
+
+        //Konstruktor alapértelmezett értékekkel
+        public Dog()
+        {
+            Color = "";
+            Name = "";
+            Breed = "";
+            Height = 0;
+            Weight = 0;
+        }
+
+        public Dog(string color, string name, string breed, double height, double weight)
+        {
+            this.Color = color;
+            this.Name = name;
+            this.Breed = breed;
+            this.Height = height;
+            this.Weight = weight;
+        }
+
+        // Metódusok (viselkedések)
+        public void WagTail()
+        {
+            Console.WriteLine($"{Name} csóválja a farkát.");
+        }
+
+        public void Shit()
+        {
+            Console.WriteLine($"{Name} kupacot gyárt.");
+        }
+
+        public void Bark()
+        {
+            Console.WriteLine($"{Name} ugat: Vau-vau.");
+        }
+
+        public void Eat()
+        {
+            Console.WriteLine($"{Name} eszik.");
+        }
+
+        public void LieDown()
+        {
+            Console.WriteLine($"{Name} lefekszik.");
+        }
+        public void Shake()
+        {
+            Console.WriteLine($"{Name} megrázza magát.");
+        }
+
+        //ToString felülírás a könnyű kiíráshoz
+        public override string ToString()
+        {
+            return $"Dog: {Name} ({Breed}), Color: {Color}, Height: {Height} cm ({Weight} kg)";
+        }
+
+
+
+        static void Main( string[] args)
+        {//Bobby objektum létrehozása
+            Dog bobby = new Dog();
+            Dog rex = new Dog("Black","Rex","German Shephard", 50.0,60.0);
+            bobby.Color = "Yellow Brown";
+            bobby.Name = "Bobby";
+            bobby.Breed = "Labrador";
+            bobby.Height = 48.18;
+            bobby.Weight = 10.89;
+
+         //Bobby tulajdonságai és viselkedése
+         Console.WriteLine(bobby); //kiírás
+
+            bobby.WagTail();
+            bobby.Bark();
+            bobby.Eat();
+            bobby.LieDown();
+            bobby.Shake();
+            bobby.Shit();
+
+         Console.WriteLine(rex); //kiírás
+
+            rex.WagTail();
+            rex.Bark();
+            rex.Eat();
+            rex.LieDown();
+            rex.Shake();
+            rex.Shit();
+        }
+    }
+}
