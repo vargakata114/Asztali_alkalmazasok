bb3a39facdb8fd75e4e8a171012540391b4ca36f
vargakata114 <vargakata819@gmail.com>
2026-03-09 10:54:59 +0100

    Add files via upload

---
+++ UnitTests/MSTest1/Test1.cs
@@ -0,0 +1,20 @@
+﻿namespace MSTest1
+{
+    [TestClass]
+    public sealed class Test1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+
+    [TestClass]
+    public sealed class Test2
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ UnitTests/xUnitTest1/UnitTest1.cs
@@ -0,0 +1,20 @@
+namespace xUnitTest1
+{
+    public class UnitTestCsoport1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+
+    public class UnitTestCsoport2
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
