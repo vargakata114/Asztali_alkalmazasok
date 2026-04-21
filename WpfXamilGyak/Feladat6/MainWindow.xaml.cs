3c7dc8db6d9c5f47ad09aa16969c68c0792f3bac
vargakata114 <vargakata819@gmail.com>
2026-04-21 14:43:49 +0200

    Add files via upload

---
+++ WpfXamilGyak/Feladat6/MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.Windows.Themes;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Feladat6
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            // Egyszerű lista létrehozása
+            var Feladatok = new List<Feladat>
+            {
+                new Feladat {Nev = "Dokumentáció", Kesz = false},
+                new Feladat {Nev = "UI design", Kesz = true},
+                new Feladat {Nev = "Backend", Kesz = false}
+            };
+
+            // ListBox ItemsSource beállítása - EZ A LEGFONTOSABB!
+            lstFeladatok.ItemsSource = Feladatok;
+        }
+    }
+
+        public class Feladat
+        {
+            public string Nev { get; set; }
+            public bool Kesz { get; set; }
+    }
+}
---
+++ WpfXamilGyak/SzemelyesAdatlapUrlap/MainWindow.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SzemelyesAdatlapUrlap
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        bool enabled {  get; set; }
+        string name { get; set; }
+        int nemek {  get; set; }
+        DateTime datum { get; set; }
+        public MainWindow()
+        {
+            InitializeComponent();
+            enabled = false;
+            name = string.Empty;
+            nemek = 0;
+            datum = DateTime.Now;
+        }
+    }
+}
